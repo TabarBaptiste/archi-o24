0d95c60e27f2ae5eaa559e3125b5140291922721
Baptiste <tabarbaptiste@gmail.com>
2025-08-18 16:49:44 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { MainComponent } from './components/main/main.component';
 
 export const routes: Routes = [
     // {
     // 	path: 'header',
     // 	component: HeaderComponent
     // },
-    { path: 'contact', component: ContactComponent }
+    { path: 'contact', component: ContactComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'main', component: MainComponent }
 ];
