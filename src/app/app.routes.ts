66482a2b18905d8db396f28f44bded940fe0ff6d
Baptiste <tabarbaptiste@gmail.com>
2025-08-18 15:06:50 +0200

    tiens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'header',
+		component: HeaderComponent
+	},
+	{
+		path: 'contact',
+		component: ContactComponent
+	}
+];
